5fc737cab45f16322993743c49150c60ec4289e4
Mackenzie <jojocraftde@proton.me>
2025-11-05 22:07:21 +0100

    Add Vercel adapter and configure redirects
    Signed-off-by: Mackenzie <jojocraftde@proton.me>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   site: 'https://mackenziedev.site',
   integrations: [mdx(), tailwind()],
+
   redirects: {
     '/github': 'https://github.com/mackenzie',
     '/linkedin': 'https://linkedin.com/in/mackenzie',
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true
     }
-  }
+  },
+
+  adapter: vercel()
 });
